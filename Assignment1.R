7eec04721198ba9aa092571b07ed0bd501c1bdaf
“Rickyj99” <riccardodalcero99@gmail.com>
2022-05-26 22:17:37 +0200

    point 3

--- Assignment1.R
+++ Assignment1.R
@@ -1,39 +1,39 @@
-#Federico Vicentini
-#23/05/2022
+# Federico Vicentini
+# 23/05/2022
 
-#Macroeconomics - Assignment no.1
+# Macroeconomics - Assignment no.1
 
 #################
-#####POINT 1#####
+##### POINT 1#####
 #################
 
-#Clear the variables
-rm(list=ls()) 
+# Clear the variables
+rm(list = ls())
 
-#Set the working directory to source file location with
+# Set the working directory to source file location with
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#Load packages
+# Load packages
 library(quantmod)
 library(eurostat)
 
-#Get Data from Fred website
-nipa=c("EXPGS","IMPGS","PCEC","GDP","GPDI","GCE")
+# Get Data from Fred website
+nipa <- c("EXPGS", "IMPGS", "PCEC", "GDP", "GPDI", "GCE")
 for (i in 1:length(nipa)) {
-  getSymbols(nipa[i],src='FRED')
+  getSymbols(nipa[i], src = "FRED")
 }
 
-#Plot residuals from NIPA equation
-((EXPGS-IMPGS+PCEC+GPDI+GCE)-GDP)^2
-plot(((EXPGS-IMPGS+PCEC+GPDI+GCE)-GDP)^2)
+# Plot residuals from NIPA equation
+((EXPGS - IMPGS + PCEC + GPDI + GCE) - GDP)^2
+plot(((EXPGS - IMPGS + PCEC + GPDI + GCE) - GDP)^2)
 
 
 # GET THE OTHER TWO COUNTRIES FROM EUROSTAT DATABASE
 
 # Install packages
-packages <- c("tidyverse","rsdmx","eurostat","tbl2xts","tidyquant","BCDating","pwt10","dplyr")
-new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+packages <- c("tidyverse", "rsdmx", "eurostat", "tbl2xts", "tidyquant", "BCDating", "pwt10", "dplyr")
+new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
 invisible(lapply(packages, library, character.only = TRUE))
 
 # Get Eurostat data listing
@@ -50,182 +50,193 @@ library(ecb)
 # http://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tsdtr210
 
 # GDP download
-namq_10_gdp <- get_eurostat("namq_10_gdp", 
-                            stringsAsFactors = FALSE)
+namq_10_gdp <- get_eurostat("namq_10_gdp",
+  stringsAsFactors = FALSE
+)
 
 # Create vector with list of countries
-nimacountries=c("ES","FR")
+nimacountries <- c("ES", "FR")
 
 # Create a vector with codes for NIMA aggregates
-nimaeu=c("B1GQ","P31_S14_S15","P3_S13","P51G","P52","P53","P6","P7")
+nimaeu <- c("B1GQ", "P31_S14_S15", "P3_S13", "P51G", "P52", "P53", "P6", "P7")
 
 # Create a vector with names for columns in matrix dataeu
-colname=c("GDP","C","G","I","inv","saldo","X","IM",
-          "GDPb","Cb","Gb","Ib","invb","saldob","Xb","IMb")
+colname <- c(
+  "GDP", "C", "G", "I", "inv", "saldo", "X", "IM",
+  "GDPb", "Cb", "Gb", "Ib", "invb", "saldob", "Xb", "IMb"
+)
 
 # Create matrix dataeu to fill with data from the 2 countries
-dataeu=matrix(NA,89,length(nimacountries)*length(nimaeu))
-colnames(dataeu)=colname
+dataeu <- matrix(NA, 89, length(nimacountries) * length(nimaeu))
+colnames(dataeu) <- colname
 
 # Set z to partition columns
-z=length(colname)/length(nimacountries) 
+z <- length(colname) / length(nimacountries)
 
 # For cycle to fill dataeu with data from the countries
 for (i in 1:length(nimacountries)) {
   for (s in 1:length(nimaeu)) {
-    newdata<-namq_10_gdp %>% 
-      filter(geo == nimacountries[i]) %>% 
-      filter(time >= '2000-01-01') %>% 
-      filter(unit == 'CP_MEUR') %>% 
-      filter(s_adj == 'SCA') %>% 
-      filter(na_item == nimaeu[s]) 
-    newdata$time<-convert_dates(newdata$time)
-    newdataxts<-xts(newdata$values,newdata$time)
-    dataeu[,(s+(z*(i-1)))]=newdataxts
+    newdata <- namq_10_gdp %>%
+      filter(geo == nimacountries[i]) %>%
+      filter(time >= "2000-01-01") %>%
+      filter(unit == "CP_MEUR") %>%
+      filter(s_adj == "SCA") %>%
+      filter(na_item == nimaeu[s])
+    newdata$time <- convert_dates(newdata$time)
+    newdataxts <- xts(newdata$values, newdata$time)
+    dataeu[, (s + (z * (i - 1)))] <- newdataxts
   }
 }
 
-#Change Imports from positive to negative in both countries
-dataeu[,z]=dataeu[,z]*(-1)
-dataeu[,length(colname)]=dataeu[,length(colname)]*(-1)
+# Change Imports from positive to negative in both countries
+dataeu[, z] <- dataeu[, z] * (-1)
+dataeu[, length(colname)] <- dataeu[, length(colname)] * (-1)
 
-#Convert dataeu to xts format
-dataeu<-xts(dataeu,rev(newdata$time))
+# Convert dataeu to xts format
+dataeu <- xts(dataeu, rev(newdata$time))
 
-#Check NIMA identity for country 1
-sums=c()
-gdp=c()
+# Check NIMA identity for country 1
+sums <- c()
+gdp <- c()
 for (i in 1:nrow(dataeu)) {
-  sums[i]=sum(dataeu[i,-c(1,(z+1):length(colname))])
-  gdp[i]=dataeu[i,1]
+  sums[i] <- sum(dataeu[i, -c(1, (z + 1):length(colname))])
+  gdp[i] <- dataeu[i, 1]
 }
 
-sum(dataeu[i,-c(1,z:length(colname))])-dataeu[i,z]
-dataeu[,-c(1,z:length(colname))]
+sum(dataeu[i, -c(1, z:length(colname))]) - dataeu[i, z]
+dataeu[, -c(1, z:length(colname))]
 
-plot(sums, col="green", type="l")
-lines(gdp, col="red", type="l")
-plot(sums-gdp)
+plot(sums, col = "green", type = "l")
+lines(gdp, col = "red", type = "l")
+plot(sums - gdp)
 
 
-#Check NIMA identity for country 2
-sums2=c()
-gdp2=c()
+# Check NIMA identity for country 2
+sums2 <- c()
+gdp2 <- c()
 for (i in 1:nrow(dataeu)) {
-  sums2[i]=sum(dataeu[i,-c(1:(z+1))])
-  gdp2[i]=dataeu[i,(z+1)]
+  sums2[i] <- sum(dataeu[i, -c(1:(z + 1))])
+  gdp2[i] <- dataeu[i, (z + 1)]
 }
 
-sum(dataeu[1,-c(1:(z+1))])-dataeu[1,length(colname)]
+sum(dataeu[1, -c(1:(z + 1))]) - dataeu[1, length(colname)]
 
-dataeu[i,(z+1)]
+dataeu[i, (z + 1)]
 
-dataeu[1,-c(1:(z+1),length(colname))]
+dataeu[1, -c(1:(z + 1), length(colname))]
 
-plot(sums2, col="green", type="l")
-lines(gdp2, col="red", type="l")
-plot(sums2-gdp2)
-
-
+plot(sums2, col = "green", type = "l")
+lines(gdp2, col = "red", type = "l")
+plot(sums2 - gdp2)
 
 
 
-plot(sums-gdp) #plot the residual to check if Y=C+G+I
+
+
+plot(sums - gdp) # plot the residual to check if Y=C+G+I
 
 
 
 
 
 #################
-#####POINT 2#####
+##### POINT 2#####
 #################
 
 
-library(BCDating) #import
+library(BCDating) # import
 
-data_bc<-matrix(NA,length(GDP),4) #generate a matrix to fill with the log of the variables
-#taking logs
-data_bc[,1]<-log(GDP)
-data_bc[,2]<-log(IMPGS)
-data_bc[,3]<-log(GPDI)
-data_bc[,4]<-log(PCEC)
+data_bc <- matrix(NA, length(GDP), 4) # generate a matrix to fill with the log of the variables
+# taking logs
+data_bc[, 1] <- log(GDP)
+data_bc[, 2] <- log(IMPGS)
+data_bc[, 3] <- log(GPDI)
+data_bc[, 4] <- log(PCEC)
 
 for (count in 1:4) {
-  #create a time series with the variable[count]
-  #setting the time index as year-quarter
-  data_ts<-ts(data_bc[,count],start = c(1947, 1), frequency = 4) 
-  #applied the BBQ method 
-  bc_US<-BBQ(data_ts,name= count)
-  #plot the results
+  # create a time series with the variable[count]
+  # setting the time index as year-quarter
+  data_ts <- ts(data_bc[, count], start = c(1947, 1), frequency = 4)
+  # applied the BBQ method
+  bc_US <- BBQ(data_ts, name = count)
+  # plot the results
   summary(bc_US)
-  plot(bc_US,data_ts)
+  plot(bc_US, data_ts)
 }
 
 
 
 
 #################
-#####POINT 3#####
+##### POINT 3#####
 #################
 rm(list = ls())
 
 library(pwt10)
 library(dplyr)
 data("pwt10.0")
-penn=pwt10.0
+penn <- pwt10.0
 rm(pwt10.0)
 
-list=c("1950","1960","1970","1980","1990","2000","2010")
+list <- c("1950", "1960", "1970", "1980", "1990", "2000", "2010")
 
-uspenn<-penn %>% 
+uspenn <- penn %>%
   filter(country == "United States of America")
-uslabsh=uspenn %>%
-  select(c(year,labsh))
+uslabsh <- uspenn %>%
+  select(c(year, labsh))
 
-espenn<-penn %>% 
+espenn <- penn %>%
   filter(country == "Spain")
-eslabsh=espenn %>%
-  select(c(year,labsh))
+eslabsh <- espenn %>%
+  select(c(year, labsh))
 
-frpenn<-penn %>% 
+frpenn <- penn %>%
   filter(country == "France")
-frlabsh=frpenn %>%
-  select(c(year,labsh))
+frlabsh <- frpenn %>%
+  select(c(year, labsh))
 
 
-usfilt=uslabsh %>%
+usfilt <- uslabsh %>%
   filter(year %in% list)
-plot(usfilt$year,usfilt$labsh, ylim=c(0,1),
-     type="b",
-     ylab="Labour Share of Total Income",
-     xlab="Year", main="US")
+plot(usfilt$year, usfilt$labsh,
+  ylim = c(0, 1),
+  type = "b",
+  ylab = "Labour Share of Total Income",
+  xlab = "Year", main = "US"
+)
 
-esfilt=eslabsh %>%
+esfilt <- eslabsh %>%
   filter(year %in% list)
-plot(usfilt$year,esfilt$labsh, ylim=c(0,1),
-     type="b",
-     ylab="Labour Share of Total Income",
-     xlab="Year", main="SPAIN")
+plot(usfilt$year, esfilt$labsh,
+  ylim = c(0, 1),
+  type = "b",
+  ylab = "Labour Share of Total Income",
+  xlab = "Year", main = "SPAIN"
+)
 
 
-frfilt=frlabsh %>%
+frfilt <- frlabsh %>%
   filter(year %in% list)
-plot(frfilt$year,frfilt$labsh, ylim=c(0,1),
-     type="b",
-     ylab="Labour Share of Total Income",
-     xlab="Year", main="FRANCE")
+plot(frfilt$year, frfilt$labsh,
+  ylim = c(0, 1),
+  type = "b",
+  ylab = "Labour Share of Total Income",
+  xlab = "Year", main = "FRANCE"
+)
 
 table_10 <- matrix(NA, ncol = 6, nrow = 10)
-countries <- c("United States of America", "France", "Germany", "Japan",
-"Canada", "China",
-"Italy", "Netherlands", "United Kingdom","Spain")
+countries <- c(
+  "United States of America", "France", "Germany", "Japan",
+  "Canada", "China",
+  "Italy", "Netherlands", "United Kingdom", "Spain"
+)
 char <- c("country", "rgdpna", "rtfpna", "rnna", "avh", "emp")
-table_10[,1] <- countries
+table_10[, 1] <- countries
 colnames(table_10) <- char
 
 
 penn_years <- penn %>%
-  filter((year >= 1959) & (year <= 2000) & (country %in% countries ))
+  filter((year >= 1959) & (year <= 2000) & (country %in% countries))
 penn_years <- select(penn_years, char)
 gdp_ratio <- gdp(penn_years[, 2])
 
@@ -233,43 +244,42 @@ gdp <- function(x) {
   x <- log(x)
   out <- c(1)
   for (count in 2:length(x)) {
-     out  <- append(out, (x[count] - x[count - 1]) / x[count - 1])
+    out <- append(out, (x[count] - x[count - 1]) / x[count - 1])
   }
   return(out)
 }
 gdp_ratio
 penn_years$gdp_ratio <- gdp(penn_years[, 2])
 #################
-#####POINT 4#####
+##### POINT 4#####
 #################
 
 
-favero=read.csv("MRW.csv",sep=";",dec = ",")
-favero$ID=NULL
+favero <- read.csv("MRW.csv", sep = ";", dec = ",")
+favero$ID <- NULL
 
-replica=data.frame(favero$YL85,favero$N6085,favero$IY)
-names(replica)=c("Ypc","PopGrowth","IonY")
-replica$Ypc=log(replica$Ypc)
-replica$PopGrowth=log(((replica$PopGrowth)/100)+0.05)
-replica$IonY=log((replica$IonY)/100)
-names(replica)[2]="ngdelta"
+replica <- data.frame(favero$YL85, favero$N6085, favero$IY)
+names(replica) <- c("Ypc", "PopGrowth", "IonY")
+replica$Ypc <- log(replica$Ypc)
+replica$PopGrowth <- log(((replica$PopGrowth) / 100) + 0.05)
+replica$IonY <- log((replica$IonY) / 100)
+names(replica)[2] <- "ngdelta"
 
 
 
-reg=lm(Ypc ~ IonY + ngdelta, data=replica)
+reg <- lm(Ypc ~ IonY + ngdelta, data = replica)
 library(stargazer)
 stargazer(reg, type = "text")
 
 
-t.test(lm(Ypc ~ IonY + ngdelta, data=replica))
+t.test(lm(Ypc ~ IonY + ngdelta, data = replica))
 
 
 library(car)
-linearHypothesis(reg, c("IonY-ngdelta=0"),test="F")
-linearHypothesis(reg, c("IonY=0.5","ngdelta=-0.5"),test="F")
+linearHypothesis(reg, c("IonY-ngdelta=0"), test = "F")
+linearHypothesis(reg, c("IonY=0.5", "ngdelta=-0.5"), test = "F")
 
 ## Classical example: Student's sleep data
 plot(extra ~ group, data = sleep)
 ## Traditional interface
 with(sleep, t.test(extra[group == 1], extra[group == 2]))
-
